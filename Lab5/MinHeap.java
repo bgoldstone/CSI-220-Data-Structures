38288a02435d660f36b94854d24c450272ffa218
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-11-17 18:06:15 -0500

    updated code

--- Lab5/MinHeap.java
+++ Lab5/MinHeap.java
@@ -1,69 +1,97 @@
 /**
-  * Creates an object of a MinHeap where the smaller the number, the higher the priority.
-*/
-public class MinHeap<T>{
-  private Node root;
-  
-  /**
-    * Constructor for a MinHeap.
-  */
-  public Heap(){
-    root = null;
-  }
-    
+ * Creates an object of a MinHeap where the smaller the number, the higher the priority.
+ */
+public class MinHeap {
+    private HeapNode root;
+
     /**
-      * Function to insert into a MinHeap.
-      * @param priority the prioriry order. Smallest is highest priority.
-      * @param data the data or value of the node.
-    */
-    public void insert(int priority, T data){
-      if(isEmpty()){
-        root = new HeapNode(int priority, T data);
-      } else{
-        
-      }
+     * Constructor for a {@link MinHeap}.
+     */
+    public MinHeap() {
+        root = null;
     }
-  }
-  
-  /**
-    * Gets if heap is Empty
-    * @return true if heap is empty.
-  */
-  public boolean isEmpty(){
-    return root == null;
-  }
 
-  /**
-    * Creates an object of a HeapNode.
-    */
-  class HeapNode{
-    int priority;
-    T data;
-    Node left;
-    Node Right;
-
-     /**
-      * Constructor for a HeapNode.
-      * @param priority the prioriry order. Smallest is highest priority.
-      * @param data the data or value of the node.
-    */
-    public HeapNode(int priority, T data){
-      this.priority = priority;
-      this.data = data;
-    }
-    
     /**
-      * Constructor for a HeapNode.
-      * @param priority the prioriry order. Smallest is highest priority.
-      * @param data the data or value of the node.
-      * @param left the current node's left node.
-      * @param right the current node's right node.
-    */
-    public HeapNode(int priority, T data, Node left, Node right){
-      this.priority = priority;
-      this.data = data;
-      this.left = left;
-      this.right = right;
+     * Function to insert into a {@link MinHeap}.
+     *
+     * @param value the value or value of the node.
+     * @param name  the name of the Guest.
+     */
+    public HeapNode insert(int value, String name) {
+        if (isEmpty()) {
+            return new HeapNode(value, name);
+        } else {
+            return null;
+        }
     }
 
+    /**
+     * Gets if {@link MinHeap} is Empty
+     *
+     * @return true if {@link MinHeap} is empty.
+     */
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    /**
+     * Displays all of the {@link HeapNode} in the {@link MinHeap}.
+     */
+    public void display() {
+        displayRecursive(root);
+    }
+
+    /**
+     * Traverses {@link MinHeap} inOrder.
+     *
+     * @param current Root node for first iteration.
+     * @return {@link HeapNode}.
+     */
+    public HeapNode displayRecursive(HeapNode current) {
+        if (root == null)
+            return current;
+        displayRecursive(current.left);
+        System.out.println("Guest Name: " + current.name + "Priority: " + current.value);
+        displayRecursive(current.right);
+        return current;
+    }
+
+    /**
+     * Creates an object of a {@link HeapNode}.
+     */
+    class HeapNode {
+        int value;
+        String name;
+        HeapNode left;
+        HeapNode right;
+
+        /**
+         * Constructor for a {@link HeapNode}.
+         *
+         * @param value the data or value of the {@link HeapNode}.
+         * @param name   the name of the Guest.
+         */
+        public HeapNode(int value, String name) {
+            this.value = value;
+            this.name = name;
+            this.left = null;
+            this.right = null;
+        }
+
+        /**
+         * Constructor for a {@link HeapNode}.
+         *
+         * @param value the data or value of the {@link HeapNode}.
+         * @param name  the name of the Guest.
+         * @param left  the node's left {@link HeapNode}.
+         * @param right the node's right {@link HeapNode}.
+         */
+        public HeapNode(int value, String name, HeapNode left, HeapNode right) {
+            this.value = value;
+            this.name = name;
+            this.left = left;
+            this.right = right;
+        }
+
+    }
 }
--- Lab5/OpenHashTable.java
+++ Lab5/OpenHashTable.java
@@ -1,107 +1,111 @@
 /**
-  * Creates an object of an OpenHashTable.
-*/
-public class OpenHashTable<T>{
-  private int D;
-  private HashTableNode[] nodeArray;
-  
-  /**
-    * Constructor for an Open Hash Table.
-    * @param D number of slots in the array. The bigger the D, the faster to find the key.
-  */
-  public OpenHashTable(int D){
-    this.D = D;
-    nodeArray = new HashTableNode[D];
-  }
-  
-  /**
-    * Inserts HashTableNode into OpenHashTable.
-    * @param key The key of the HashTableNode to insert.
-    * @param value The value of the HashTableNode to insert.
-  */
-  public void insert(T key, T value){
-    HashTableNode current = nodeArray[((int) key) % D];
-    if(current != null){
-      while(current.next != null){
-        current = current.next;
-      }
-    current.next = new HashTableNode(key, value);
-    }else{
-      current = new HashTableNode(key, value);
-    }
-  }
-  
-  /**
-    * Finds ot key exists in the OpenHashTable.
-    * @param key key to find in OpenHashTable.
-    * @return true if item is in the OpenHashTable.
-  */
-  public boolean search(T key){
-    HashTableNode current = d[((int) key) % D];
-    if(current == null){
-      return false;
-    }else{
-      while(current.next != null){
-        if(current.value.equals(value))
-          return true;
-        else{
-          current = current.next
-        }
-      }
-      return false;
-  }
-  
-  /**
-    * Displays the whole OpenHashTable.
-  */
-  public void display(){
-    int currentD = 0;
-    for (HashTableNode current : nodeArray){
-      System.out.println("Row " + currentD + ":");
-      if(current != null){
-        while(current != null){
-          System.out.print(current.value);
-          current = current.next;
-        }
-        System.out.println();
-      }
-      currentD++;
-    }
-  }
-  
-  /**
-    * Removes the given HashNode from the OpenHashTable.
-    * @param key key to remove from the OpenHashTable.
-  */
-  public boolean remove(T key){
-    HashTableNode current = d[((int) key) % D];
-    while(current != null){
-      if(current.next.value.equals(value)){
-        current.next = current.next.next;
-        return true;
-      }
-    }
-    return false;
-  }
-  
-  /**
-    * Creates an object of a HashTableNode.
-  */
-  class HashTableNode<T>{
-    T key;
-    T value;
-    HashTableNode next;
-    
+ * Creates an object of an OpenHashTable.
+ */
+public class OpenHashTable<T> {
+    private final int D;
+    private final HashTableNode[] nodeArray;
+
     /**
-      * Constructor for a HashTableNode.
-      * @param key key to use in HashTableNode.
-      * @param value value to use in HashTableNode.
-    */
-    public HashTableNode(T key, T value){
-      this.key = key;
-      this.value = value;
-      this.next = null;
+     * Constructor for an Open Hash Table.
+     *
+     * @param D number of slots in the array. The bigger the D, the faster to find the key.
+     */
+    public OpenHashTable(int D) {
+        this.D = D;
+        nodeArray = new HashTableNode[D];
     }
-  }
+
+    /**
+     * Inserts HashTableNode into OpenHashTable.
+     *
+     * @param key   The key of the HashTableNode to insert.
+     * @param value The value of the HashTableNode to insert.
+     */
+    public void insert(T key, T value) {
+        HashTableNode current = nodeArray[((int) key) % D];
+        if (current != null) {
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = new HashTableNode(key, value);
+        } else {
+            current = new HashTableNode(key, value);
+        }
+    }
+
+    /**
+     * Finds ot key exists in the OpenHashTable.
+     *
+     * @param key key to find in OpenHashTable.
+     * @return true if item is in the OpenHashTable.
+     */
+    public boolean search(T key) {
+        HashTableNode current = nodeArray[((int) key) % D];
+        if (current != null) {
+            while (current.next != null) {
+                if (current.value.equals(key))
+                    return true;
+                else {
+                    current = current.next;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Displays the whole OpenHashTable.
+     */
+    public void display() {
+        int currentD = 0;
+        for (HashTableNode current : nodeArray) {
+            System.out.println("Row " + currentD + ":");
+            if (current != null) {
+                while (current != null) {
+                    System.out.print(current.value);
+                    current = current.next;
+                }
+                System.out.println();
+            }
+            currentD++;
+        }
+    }
+
+    /**
+     * Removes the given HashNode from the OpenHashTable.
+     *
+     * @param key key to remove from the OpenHashTable.
+     */
+    public boolean remove(T key) {
+        HashTableNode current = nodeArray[((int) key) % D];
+        while (current != null) {
+            if (current.next.value.equals(key)) {
+                current.next = current.next.next;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Creates an object of a HashTableNode.
+     */
+    class HashTableNode<T> {
+        T key;
+        T value;
+        HashTableNode next;
+
+        /**
+         * Constructor for a HashTableNode.
+         *
+         * @param key   key to use in HashTableNode.
+         * @param value value to use in HashTableNode.
+         */
+        public HashTableNode(T key, T value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+        }
+    }
 
 }
