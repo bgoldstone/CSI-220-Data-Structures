2f5ffae6c963b5ed43d8759a28a5bda52d194c50
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-11-18 15:16:07 -0500

    added AdjacencyMatrix

---
+++ ClassroomExamples/src/AdjacencyMatrix.java
@@ -0,0 +1,14 @@
+public class AdjacencyMatrix {
+    static int[][] map;
+    public void insertEdge(int a, int b, int weight){
+
+    }
+
+    public void inDegreeTotal(int a){
+
+    }
+
+    public void outDegreeTotal(int a){
+
+    }
+}
