be6e9da2ed5da1c8a9c44a4ec1718b197c785d29
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-12-10 11:11:12 -0500

    added documentation to Stack.java

--- Lab6/Stack.java
+++ Lab6/Stack.java
@@ -1,54 +1,95 @@
+/**
+ * An implementation of a Stack using a LinkedList.
+ */
 public class Stack {
     private LinkedList stack;
     private int maxSize;
 
+    /**
+     * A constructor for a {@link Stack}.
+     *
+     * @param maxSize Maximum size of a {@link Stack}.
+     */
     public Stack(int maxSize) {
         stack = new LinkedList();
         this.maxSize = maxSize;
 
     }
 
+    /**
+     * Pushes an item into a Stack.
+     *
+     * @param number number to push in the Stack.
+     */
     public void push(int number) {
         stack.addFirst(number);
 
     }
 
+    /**
+     * Pops an item into a Stack.
+     *
+     * @return Removed item from the Stack.
+     */
     public int pop() {
         if (isEmpty())
             return -1;
         return stack.removeFirst();
     }
 
+    /**
+     * Peeks first item from the Stack.
+     *
+     * @return First item from the Stack.
+     */
     public int peek() {
         if (isEmpty())
             return -1;
         return stack.getFirst();
     }
 
+    /**
+     * Checks if Stack is empty.
+     *
+     * @return true if empty.
+     */
     public boolean isEmpty() {
         return stack.isEmpty();
     }
 
+    /**
+     * Checks if Stack is Full.
+     */
     public boolean isFull() {
         return stack.size() == maxSize;
     }
 
+    /**
+     * Clears the stack.
+     */
     public void clear() {
         stack.clear();
     }
 }
 
+/**
+ * An implementation of a LinkedList.
+ */
 class LinkedList {
     private Node top; // Pointer to the head of the list.
 
+    /**
+     * Constructor for a {@link LinkedList}.
+     */
     public LinkedList() {
         top = null;
     }
 
-    /*
+    /**
      * Enter a data item at the front of the list.
+     *
+     * @param item item to add to the front of the list.
      */
-
     public void addFirst(int item) {
         Node ptr = new Node(); // Construct a new node - ptr points to it.
         ptr.data = item; // New node's data is the parameter item.
@@ -58,10 +99,10 @@ class LinkedList {
         top = ptr;
     }
 
-    /*
-     * size()
+    /**
+     * Gets Size of the list.
      *
-     * Return the length of the list.
+     * @return the length of the list.
      */
 
     public int size() {
@@ -77,9 +118,10 @@ class LinkedList {
         return count; // Return the number of nodes.
     }
 
-    /*
-     * removeFirst()
-     * removes first element
+    /**
+     * Removes the first element.
+     *
+     * @return first element.
      */
     public int removeFirst() {
         Node temp = top;
@@ -87,27 +129,28 @@ class LinkedList {
         return temp.data;
     }
 
-    /*
-     * getFirst()
-     * returns first element data
+    /**
+     * Gets the first Element
+     *
+     * @return the first element data
      */
     public int getFirst() {
         return top.data;
     }
 
-    /*
-     * isEmpty()
-     * returns true if list is empty
+    /**
+     * Checks if list is empty.
+     *
+     * @return true if list is empty
      * false if list is not
      */
     public boolean isEmpty() {
         return top == null;
     }
 
-    /*
+    /**
      * Display the contents of the linked list.
      */
-
     public void display() {
         Node ptr = top; // Pointer to traverse the list.
 
@@ -120,10 +163,16 @@ class LinkedList {
         System.out.println();
     }
 
+    /**
+     * Clears the LinkedList.
+     */
     public void clear() {
         top = null;
     }
 
+    /**
+     * Node Class.
+     */
     private class Node {
         public int data; // Data stored in the Node.
         public Node next; // Pointer to next Node in the list.
